7b149eb05f70a55389e50e6321d81c7c7e916d1a
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-05-08 22:39:52 +0200

    Showing how to establish web socket connection

--- WebSocketServer/WebSocketServer/Program.cs
+++ WebSocketServer/WebSocketServer/Program.cs
@@ -2,33 +2,45 @@ using System.Net.WebSockets;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//builder.Services.AddControllers();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
 
 app.UseWebSockets();
 
 app.Use(async (context, next) =>
 {
-   if(context.WebSockets.IsWebSocketRequest)
+    WriteRequestParam(context);
+   if (context.WebSockets.IsWebSocketRequest)
     {
         WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
         Console.WriteLine("WebSocket Connected");
     }
    else
     {
+        Console.WriteLine("Hello from the 2nd request delegate.");
         await next();
     }
 });
 
+app.Run(async context =>
+{
+    Console.WriteLine("Hello from the 3rd request delegate.");
+    await context.Response.WriteAsync("Hello from the 3rd request delegate.");
+});
+
 app.Run();
+
+
+void WriteRequestParam(HttpContext context)
+{
+    Console.WriteLine("Request Method: " + context.Request.Method);
+    Console.WriteLine("request Protocol: " + context.Request.Protocol);
+
+    if(context.Request.Headers != null)
+    {
+        foreach(var h in context.Request.Headers)
+        {
+            Console.WriteLine("--> " + h.Key + " : " + h.Value);
+        }
+    }
+}
+
