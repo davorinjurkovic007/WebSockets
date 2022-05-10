ae805d445183bc537ca41ad1ce2baaf36ec2c0c0
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-05-10 17:48:23 +0200

    Faze 2 finished

--- WebSocketServer/WebSocketServer/Program.cs
+++ WebSocketServer/WebSocketServer/Program.cs
@@ -13,6 +13,20 @@ app.Use(async (context, next) =>
     {
         WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
         Console.WriteLine("WebSocket Connected");
+
+        await ReceiveMessage(webSocket, async (result, buffer) =>
+        {
+            if(result.MessageType == WebSocketMessageType.Text)
+            {
+                Console.WriteLine("Message Received");
+                return;
+            }
+            else if(result.MessageType == WebSocketMessageType.Close)
+            {
+                Console.WriteLine("Recived Close message");
+                return;
+            }
+        });
     }
    else
     {
@@ -43,4 +57,18 @@ void WriteRequestParam(HttpContext context)
         }
     }
 }
+
+async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]>handleMessage)
+{
+    var buffer = new byte[1024 * 4];
+
+    while(socket.State == WebSocketState.Open)
+    {
+        var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer), cancellationToken: CancellationToken.None);
+
+        handleMessage(result, buffer);
+    }
+}
+
+
 
